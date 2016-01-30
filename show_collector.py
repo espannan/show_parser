4760a718795b1da5a4cf7d451f349a48c1933a3e
espannan <espannan@gmail.com>
2016-01-29 23:31:10 -0800

    add fillmore

--- show_collector.py
+++ show_collector.py
@@ -7,7 +7,8 @@ shows = {}
 
 for venue, info in venues.items():
     parser = __import__('venue_parsers.' + info['parser'], fromlist=[info['parser']])
-    page = requests.get(info['url']).content
+    headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30'}
+    page = requests.get(info['url'], headers=headers).content
     shows[venue] = parser.parse(page)
 
 for venue, venue_shows in shows.items():
---
+++ venue_parsers/fillmore.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import re
+
+def parse(page):
+    soup = BeautifulSoup(page, 'html.parser')
+    print page
+
+    shows = []
+
+    for entry in soup.find_all('div', class_='faq_main calendar'):
+        parts = entry.findChild('p', class_='title').text.split('with')
+
+        show = {'headliner' : parts[0]}
+
+        if len(parts) > 1:
+            show['openers'] = parts[1].strip()
+
+        time_info = entry.findChild('span', class_='content').findChild('p')
+
+        date = time_info.next_element
+
+        show['date'] = str(date)
+
+        show['time'] = str(date.next_element.next_element)
+
+        shows.append(show)
+
+    return shows
+
