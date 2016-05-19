ec481b2a1fe788610d8dc7f182b530209294321f
Erin Spannan <erin@minervaproject.com>
2016-05-19 10:07:47 -0700

    update page download script to be able to get non-javascript pages

--- download_page.py
+++ download_page.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from collector import Collector
+import requests
 
 config_file = raw_input('Config file: ')
 parser_directory = raw_input('Parser directory: ')
@@ -8,7 +9,13 @@ collector = Collector(config_file, parser_directory)
 
 parser = raw_input('Parser name: ')
 
-page = collector.get_loaded_page(collector.sites[parser]['url'])
+javascript = raw_input('Javascript y/N: ')
+
+if javascript.lower() == 'y':
+    page = collector.get_loaded_page(collector.sites[parser]['url'])
+else:
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30'}
+    page = requests.get(collector.sites[parser]['url'], headers=headers)
 
 f = open('debug_sites/%s.html' % parser, 'w')
 f.write(page)
