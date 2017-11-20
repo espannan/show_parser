bb33faed41dbf0daf449f4bb7bab4ac27bf82b3c
Erin Spannan <erin@minervaproject.com>
2017-11-19 23:37:36 -0800

    update the api

--- show_parser.py
+++ show_parser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from collector import Collector
+import json
+import requests
 import sys
 
 debug = len(sys.argv) > 1 and sys.argv[1] == '-d'
@@ -8,6 +10,7 @@ collector = Collector('config/venues.yml', 'venue_parsers', debug)
 
 shows = collector.scrape_sites()
 
+# print in console
 for venue, venue_shows in shows.items():
     print venue
     for show in venue_shows:
@@ -15,3 +18,23 @@ for venue, venue_shows in shows.items():
         if 'openers' in show:
             print '\t\t', show['openers']
         print '\t\t', show['showtime']
+
+
+def get_venue(url, venue_list):
+    for venue in venue_list:
+        if venue['url'] == url:
+            return venue
+
+    return None
+
+
+# update via the API
+r = requests.get('http://127.0.0.1:8000/show-venues/')
+venue_list = json.loads(r.content)
+for venue_tag, venue_shows in shows.items():
+    url = collector.sites[venue_tag]['url']
+    venue = get_venue(url, venue_list)
+
+    for show in venue_shows:
+        show['show_venue'] = venue['id']
+        r = requests.post('http://127.0.0.1:8000/shows/', data=show)
